12748e923a2f0ffb5e36433d7f036ce4e5db8f44
wjflyhigh <wjflyhigh@126.com>
2015-07-05 18:27:29 -0700

    Modify the check_round.c

--- latency/check_round.c
+++ latency/check_round.c
@@ -37,53 +37,58 @@ int main()
 	int count = 20;
 	int tmp_bak;
 
-printf("-----------------------------\n");
-while(count--)
-{
-	for(i = 0; i < array_size; i++)
-		array[i] = i;
-
-	for (i = array_size-1; i > 1; i--)
+	printf("-----------------------------\n");
+	while(count--)
 	{
-		j = rand()%i;
-//		printf("j = %d\n", j);
-		tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-	}
-
-	i = 1;
-	tmp = array[0];
-	array_check[0] = tmp;
-	while(i < array_size) // check no loop in the array[]
-	{
-		tmp_bak = tmp;
-		tmp = array[tmp];
-		for(j = 0; j < i; j++)
-		{
-			if(array_check[j] == tmp)
-			{
-				exchange_element(array,find_one_noloop_element(array, array_check, array_size, i),tmp_bak);
-				tmp = array[tmp_bak];
-			}
-				
-		}
-		array_check[i] = tmp;
-		i++;
-	}
+		for(i = 0; i < array_size; i++)
+			array[i] = i;
 	
-	for (i = 0; i < array_size; i++)
-		printf("%d ", array_check[i]);	
-	printf("\n");
-	printf("-----------------------------\n");
-/*
-	for(i = 0; i < array_size; i++)
-		printf("%d ", array[i]);
-
-	printf("\n");
-	printf("-----------------------------\n");
-*/
-}
+		for (i = array_size-1; i > 1; i--)
+		{
+			j = rand()%i;
+	//		printf("j = %d\n", j);
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+		}
+	
+		for (i = 0; i < array_size; i++)
+			printf("%d ", array[i]);	
+		printf("\n");
+		
+	
+		i = 1;
+		tmp = array[0];
+		array_check[0] = tmp;
+		while(i < array_size) // check no loop in the array[]
+		{
+			tmp_bak = tmp;
+			tmp = array[tmp];
+			for(j = 0; j < i; j++)
+			{
+			/* test whether have a round in the pointer chasing array, if it does have, pick up one element which not in array_check[], to exchange which array[tmp_bak] */
+				if(array_check[j] == tmp)
+				{
+					exchange_element(array,find_one_noloop_element(array, array_check, array_size, i),tmp_bak);
+					tmp = array[tmp_bak];
+				}
+					
+			}
+			array_check[i] = tmp;
+			i++;
+		}
+		
+	
+		for(i = 0; i < array_size; i++)
+			printf("%d ", array[i]);
+	
+		printf("\n");
+	
+		for (i = 0; i < array_size; i++)
+			printf("%d ", array_check[i]);	
+		printf("\n");
+		printf("-----------------------------\n");
+	}
 	return 0;
 }
 
